cf3b0966899cd4e0abd0cb62a3c37ebe23e10b97
racheljeong <jdsss2634@gmail.com>
2024-02-22 21:08:04 +0900

    tracking

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+// 오류 수정 후 테스트 중 
 root.render(
   <React.StrictMode>
     <App />
