7a838d16b48ee26d7e86ca20f4e39935f416dc26
racheljeong <jdsss2634@gmail.com>
2024-02-23 18:26:53 +0900

    building todo list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useState } from "react";
 
 function App() {
+
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const changeTodo = (event) => {
+    console.log(event.target.value);
+    setTodo(event.target.value);
+  }
+
+  const todoSubmit = (event) => {
+    event.preventDefault();
+    if(todo === "") return;
+    setTodoList((current) => [todo, ...todoList]);
+    setTodo("");
+  } 
+
   return (
-    <div><h1>Hello React</h1></div>
+    <div>
+      <h1>To do List {todoList.length}</h1>
+      <form onSubmit={todoSubmit}>
+        <input onChange={changeTodo} type="text" value={todo} placeholder="What to do" />
+        <button>Add</button>
+      </form>
+      <hr/>
+      <ul>
+        {todoList.map((index, each) => (
+          <li key={index}>{each}</li>
+        ))}
+      </ul>
+      </div>
   );
 }
 
